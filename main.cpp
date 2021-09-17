35be7769c107e0b56ea1d900c32a8e59e612d898
PowerBall253 <brunoanconasala@gmail.com>
2021-09-17 17:02:51 -0500

    Fix normal type detection

--- main.cpp
+++ main.cpp
@@ -94,7 +94,7 @@ int main(int argc, char **argv)
 
         // Get texture material type
         std::string ddsFileName = fs::path(argv[i]).filename().string();
-        ddsFileName = ddsFileName.substr(0, ddsFileName.find('.'));
+        ddsFileName = ddsFileName.substr(0, ddsFileName.find_first_of(".$")); // Strip extensions and $ properties
 
         if (endsWith(ddsFileName, "_n")) {
             bimHeader.textureMaterialKind = TMK_NORMAL;
